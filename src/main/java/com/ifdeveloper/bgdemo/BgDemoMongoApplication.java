151bd3bbf4dd0d13eba0378bed5df62a213fca86
Felipe Maia <felipe.silwa@gmail.com>
2020-08-18 02:11:21 -0300

    Criação projeto spring com mongodb

---
+++ src/main/java/com/ifdeveloper/bgdemo/BgDemoMongoApplication.java
@@ -0,0 +1,13 @@
+package com.ifdeveloper.bgdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgDemoMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgDemoMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifdeveloper/bgdemo/BgDemoMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifdeveloper.bgdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgDemoMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
